948aa38086c73b5ff9b75c017844ebb50f5e9b8b
GatalskiAnton <ag629331@gmail.com>
2023-03-18 20:51:37 +0300

    added mysql connection

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import by.fpmibsu.PCBuilder.entity.component.Motherboard;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.*;
 
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/pcbuilder", "root", "root");
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String login = resultSet.getString("login");
+            String password = resultSet.getString("password");
+            System.out.println(id + " " + login + " " + password);
+        }
     }
 }
