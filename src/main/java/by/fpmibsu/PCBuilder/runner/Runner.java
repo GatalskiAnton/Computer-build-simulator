3307b99e06b5c4cbd238637cc14e403b1eac5e39
AntonGatalski <ag629331@gmail.com>
2023-05-29 10:02:11 +0300

    added logger

--- src/main/java/by/fpmibsu/PCBuilder/runner/Runner.java
+++ src/main/java/by/fpmibsu/PCBuilder/runner/Runner.java
@@ -10,10 +10,17 @@ import by.fpmibsu.PCBuilder.entity.User;
 import by.fpmibsu.PCBuilder.entity.component.Cooler;
 import by.fpmibsu.PCBuilder.entity.component.utils.Authentication;
 import by.fpmibsu.PCBuilder.entity.component.utils.Socket;
+import by.fpmibsu.PCBuilder.service.CoolerService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Runner {
     public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, DaoException {
-        System.setProperty("log4j.configurationFile", "file:\\\\C:\\Users\\ag629\\IdeaProjects\\tomcat\\src\\main\\resources\\log4j2.xml");
+        System.setProperty("log4j.configurationFile", ".\\src\\main\\resources\\log4j2.xml");
+        Logger log = LogManager.getLogger(CoolerService.class);
+
+        log.error("error");
+
         UserDao userDao = new UserDao();
         System.out.println(userDao.findUserByLogin("Eugene"));
     }
